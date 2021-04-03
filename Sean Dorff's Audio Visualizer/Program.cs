28051b906711ce5d42a0aa9d9663eb36145a4dd4
SeanDorff <seandorff@googlemail.com>
2021-04-03 20:14:43 +0200

    Add project files.

---
+++ Sean Dorff's Audio Visualizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sean_Dorff_s_Audio_Visualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
